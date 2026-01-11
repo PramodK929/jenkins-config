cff55e5bd4570256ced38b307752baa8ecc5ff11
Pramod K <sunkad.pramod@gmail.com>
2026-01-11 18:20:46 +0530

    Initial hello world spring boot app

---
+++ src/test/java/jenkins_config/JenkinsConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package jenkins_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
